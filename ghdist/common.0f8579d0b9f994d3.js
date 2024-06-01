5a38ab415c5edb7eecd9f6c6b12a7d08c3e97e94
Volodymyr Volobuiev <bobbygtx@gmail.com>
2024-06-01 17:39:00 +0200

    Production commit

---
+++ ghdist/common.0f8579d0b9f994d3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkontrolwork22=self.webpackChunkkontrolwork22||[]).push([[592],{62:(a,e,r)=>{r.d(e,{v:()=>c});var n=r(8256);let c=(()=>{class o{constructor(){this.product=""}}return o.\u0275fac=function(u){return new(u||o)},o.\u0275prov=n.Yz7({token:o,factory:o.\u0275fac,providedIn:"root"}),o})()}}]);
